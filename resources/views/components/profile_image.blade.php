7bf9345f2c9ea2306daadb6379614bb008fb1abe
gabesz <gabesz@gabesz-pop-oja.fritz.box>
2022-07-07 16:01:16 +0200

    profile_image component created

---
+++ resources/views/components/profile_image.blade.php
@@ -0,0 +1,3 @@
+<div {{ $attributes->merge(['class' => '']) }}>
+    <img src="{{ $image }}" alt="Profile Image" height="{{ $size }}" width="{{ $size }}" style="object-fit:cover; border-radius:50%;">
+</div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,17 +5,10 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
+                <x-profile_image
+                    image="https://wtwp.com/wp-content/uploads/2015/06/placeholder-image.png"
+                    size="50"
+                />
             </div>
         </div>
     </div>
